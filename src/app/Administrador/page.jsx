a21cf7a12fe4600c0b64dad26d56a2aafba0e6c5
Benit0Trdlla <benilorenzo007op@gmail.com>
2024-04-06 17:56:53 -0300

    Connection with Google Sheets. Add Bootstrap & PWA

--- src/app/Administrador/page.jsx
+++ src/app/Administrador/page.jsx
@@ -1,3 +1,47 @@
-export default function Admin() {
-    return <div>Admin</div>;
+import { parse } from 'papaparse';
+
+const fetchPreguntas = async () => {
+    const response = await fetch("https://docs.google.com/spreadsheets/d/e/2PACX-1vSuLH4t-YFmRI5phxbxvBEo9uRQaRP-P70Kpoa3PT7ZwRPWmiJdEwduLz5bKkSBWxGQV2ynDHHWIpYJ/pub?output=csv", { next: { revalidate: 30 } });
+    const csvData = await response.text();
+    const jsonData = parse(csvData, { header: true });
+    return jsonData.data;
+}
+export default async function AdminPage() {
+    const products = await fetchPreguntas();
+
+    return (
+        <>
+            <h4 className='me-2 ms-2'>Fetching de preguntas</h4>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Pregunta</th>
+                        <th scope="col">Enunciado</th>
+                        <th scope="col">A</th>
+                        <th scope="col">B</th>
+                        <th scope="col">C</th>
+                        <th scope="col">D</th>
+                        <th scope="col">Respuestas</th>
+                        <th scope="col">Explicacion</th>
+                        <th scope="col">Imagen</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {products.map((product) => (
+                        <tr key={product.Pregunta}>
+                            <td>{product.Pregunta}</td>
+                            <td>{product.Enunciado}</td>
+                            <td>{product.A}</td>
+                            <td>{product.B}</td>
+                            <td>{product.C}</td>
+                            <td>{product.D}</td>
+                            <td>{product.Respuestas}</td>
+                            <td>{product.Explicación}</td>
+                            <td><img src={product.Imagen} alt="Logo Caribe Enseña" /></td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,60 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h2>Hello</h2>
+    <main >
+      <ul class="nav bg-dark">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled text-white" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+      <h2 className="text-warning">Hello</h2>
+      <div class="accordion" id="accordionExample">
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+              Accordion Item #1
+            </button>
+          </h2>
+          <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the first items accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. Its also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+              Accordion Item #2
+            </button>
+          </h2>
+          <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the second items accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. Its also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+        <div class="accordion-item">
+          <h2 class="accordion-header">
+            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              Accordion Item #3
+            </button>
+          </h2>
+          <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+            <div class="accordion-body">
+              <strong>This is the third items accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. Its also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
